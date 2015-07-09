0105d9bc3e9ccaade48497da1aa03e5374af0aec
Guicai.Li <guicai.li.china@gmail.com>
2015-07-09 21:30:31 +0800

    update 指定目录搜索

--- MobClick.sh
+++ MobClick.sh
@@ -2,7 +2,18 @@
 
 #TODO:埋点
 
-dir=`pwd`
-path=${dir##*/}
+currentProject() {
+	#当前目录下
+	dir=`pwd`
+	path=${dir##*/}
+	grep -r 'MobClick event:' $path/* | awk -F '"' '{print $2}' | sed -e '/^$/d'> MobClick.csv
+}
 
-grep -r 'MobClick event:' $path/* | awk -F '"' '{print $2}' | sed -e '/^$/d'> MobClick.csv
+if [ $# != 1 ];then
+    currentProject
+    exit -1
+fi
+	
+#指定目录下搜索（与MobClick.sh同级目录）
+grep -r 'MobClick event:' $1/* | awk -F '"' '{print $2}' | sed -e '/^$/d'> MobClick.csv
+
